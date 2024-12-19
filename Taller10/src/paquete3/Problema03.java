6cf4090877e44ec784cd0a27c64d16cdae5f8e3c
Pablo Ordóñez <pablosebas61@gmail.com>
2024-12-19 12:29:18 -0500

    Cambios

--- Taller10/src/paquete3/Problema03.java
+++ Taller10/src/paquete3/Problema03.java
@@ -10,5 +10,36 @@ package paquete3;
  * @author reroes
  */
 public class Problema03 {
-    
+
+    public static void main(String[] args) {
+        String[][] notasCualitativas = new String[3][3];
+        double[][] notasCuantitativas = {{7.1, 8, 10}, {3, 7, 9}, {10, 9, 2.1}};
+
+        for (int i = 0; i < notasCuantitativas.length; i++) {
+            for (int j = 0; j < notasCuantitativas[i].length; j++) {
+                double nota = notasCuantitativas[i][j];
+                
+                if (nota >= 0 && nota <= 2.9) {
+                    notasCualitativas[i][j] = "Insuficiente";
+                } else if (nota >= 3 && nota <= 4.9) {
+                    notasCualitativas[i][j] = "Regular";
+                } else if (nota >= 5 && nota <= 7.9) {
+                    notasCualitativas[i][j] = "Buena";
+                } else if (nota >= 8 && nota <= 9.5) {
+                    notasCualitativas[i][j] = "Muy Buena";
+                } else if (nota >= 9.6 && nota <= 10) {
+                    notasCualitativas[i][j] = "Sobresaliente";
+                }
+                double promedio =0;
+                promedio = notasCuantitativas[i][j]/3;
+            }
+        }
+        for (int i = 0; i < notasCualitativas.length; i++) {
+            for (int j = 0; j < notasCualitativas[i].length; j++) {
+                System.out.println(notasCuantitativas[i][j]
+                        + " " + notasCualitativas[i][j]);
+            }
+        }
+    }
+        
 }
--- Taller10/src/paquete5/Problema05.java
+++ Taller10/src/paquete5/Problema05.java
@@ -10,5 +10,35 @@ package paquete5;
  * @author reroes
  */
 public class Problema05 {
-    
+
+    public static void main(String[] args) {
+        String[][] estudiantes = {
+            {"Sarah Ward", "Philip Payne"},
+            {"Carrie Burton", "Lauren Rice"},
+            {"Emma Escobar", "Lori Flores"},
+            {"Steven West", "Toni Martin"}
+        };
+
+       
+        String acumulador = "";
+
+        // Recorrer el arreglo de estudiantes
+        for (int i = 0; i < estudiantes.length; i++) {
+            for (int j = 0; j < estudiantes[i].length; j++) {
+                String estudiante = estudiantes[i][j];
+
+               
+                char letra = estudiante.charAt(0);
+                if (letra == 'S' || letra == 'P' || letra == 'T') {
+                   
+                    acumulador = acumulador + estudiante + "\n";
+                }
+            }
+        }
+
+        
+        System.out.println(acumulador);
+    }
 }
+
+
